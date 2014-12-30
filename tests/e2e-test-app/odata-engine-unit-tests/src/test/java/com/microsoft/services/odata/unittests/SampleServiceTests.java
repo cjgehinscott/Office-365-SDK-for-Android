0db377e38f9e85ce98c5479ffcf48634130cd7b1
Marcos Torres <marcost@lagash.com>
2014-12-30 14:19:47 -0300

    removing not required libraries from build.gradle and adding dependencies for wiremock

--- tests/e2e-test-app/odata-engine-unit-tests/src/test/java/com/microsoft/services/odata/unittests/SampleServiceTests.java
+++ tests/e2e-test-app/odata-engine-unit-tests/src/test/java/com/microsoft/services/odata/unittests/SampleServiceTests.java
@@ -308,7 +308,6 @@ public class SampleServiceTests extends WireMockTestBase {
         assertThat(result.size(), is(equalTo(1)));
     }
 
-    @Ignore
     @Test
     public void testDefaultHeaders() throws ExecutionException, InterruptedException {
         //testDefaultHeaders.json
